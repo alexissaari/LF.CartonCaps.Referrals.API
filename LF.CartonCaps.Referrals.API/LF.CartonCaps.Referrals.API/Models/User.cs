835daef0cfb80f8a1cd90c7a79a48d9ca366d350
Alexis Saari <alexissaari@gmail.com>
2025-04-11 13:10:16 -0500

    add models

---
+++ LF.CartonCaps.Referrals.API/LF.CartonCaps.Referrals.API/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace LF.CartonCaps.Referrals.API.Models
+{
+    public class User
+    {
+        public required int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IList<Referral> MyReferrals { get; set; }
+    }
+}
--- LF.CartonCaps.Referrals.API/LF.CartonCaps.Referrals.API/Services/ReferralsService.cs
+++ LF.CartonCaps.Referrals.API/LF.CartonCaps.Referrals.API/Services/ReferralsService.cs
@@ -1,20 +1,22 @@
-﻿namespace LF.CartonCaps.Referrals.API.Services
+﻿using LF.CartonCaps.Referrals.API.Models;
+
+namespace LF.CartonCaps.Referrals.API.Services
 {
     // Services are where we handle our business logic.
     // The logic done here is the same regardless of what data we're preforming logic rules on,
     // so let's make this static, because we don't need multiple instances of it.
     public static class ReferralsService
     {
-        static List<string> MyReferrals { get; }
+        static List<Referral> MyReferrals { get; }
 
         static ReferralsService()
         {
             MyReferrals = [
-                "Test",
-                "Test2"
+                new Referral(){ FirstName = "Alexis", LastName = "Saari", ReferralStatus = ReferralStatus.Complete },
+                new Referral(){ FirstName = "Jane", LastName = "Doe", ReferralStatus = ReferralStatus.Sent }
                 ];
         }
 
-        public static List<string> GetMyReferrals() => MyReferrals;
+        public static List<Referral> GetMyReferrals() => MyReferrals;
     }
 }
